a8d12795c476cab2279f84062868dbddd2ed9d4c
vikramsingh <mr.vikram004@gmail.com>
2023-04-08 15:44:45 +0400

    This is a combination of 2 commits.
    wrapper added for success or fail, koin di dependency added

--- src/main/kotlin/dev/reprator/plugins/Routing.kt
+++ src/main/kotlin/dev/reprator/plugins/Routing.kt
@@ -1,20 +1,44 @@
 package dev.reprator.plugins
 
+import dev.reprator.dao.DAOLanguageFacade
+import dev.reprator.dao.DAOLanguageFacadeCacheImpl
+import dev.reprator.dao.DAOLanguageFacadeImpl
+import dev.reprator.wrapper.AppEmptyException
+import dev.reprator.wrapper.FailResponse
+import dev.reprator.wrapper.ResultResponse
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import java.io.File
 
 fun Application.configureRouting() {
+
+    val dao: DAOLanguageFacade = DAOLanguageFacadeCacheImpl(
+        DAOLanguageFacadeImpl(),
+        File(environment.config.property("storage.ehcacheFilePath").getString())
+    )
+
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+            if(cause is AppEmptyException)
+                call.respond(FailResponse(HttpStatusCode.NoContent.value, cause.message))
+            else
+                call.respond(FailResponse(HttpStatusCode.InternalServerError.value, "500: ${cause.message}"))
         }
     }
+
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+
+        route("language") {
+            get {
+                val languageList = dao.allLanguage()
+                if(languageList.isEmpty())
+                    throw AppEmptyException()
+                else
+                    call.respond(ResultResponse(HttpStatusCode.OK.value, languageList))
+            }
         }
     }
 }
