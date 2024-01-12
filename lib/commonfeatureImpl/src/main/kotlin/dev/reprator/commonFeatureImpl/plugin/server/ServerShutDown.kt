bb4ba78ff48c667bb8e28666a092aff665a42531
Vikram Singh <mr.vikram004@gmail.com>
2024-01-12 16:25:00 +0400

    shutdown corrected but couple of issues found: 1) no logs when server is running 2)unable to run test, as i am unable to provide the application config

--- lib/commonfeatureImpl/src/main/kotlin/dev/reprator/commonFeatureImpl/plugin/server/ServerShutDown.kt
+++ lib/commonfeatureImpl/src/main/kotlin/dev/reprator/commonFeatureImpl/plugin/server/ServerShutDown.kt
@@ -1,20 +1,15 @@
 package dev.reprator.commonFeatureImpl.plugin.server
 
-import dev.reprator.base.action.AppDatabaseFactory
-
+import dev.reprator.commonFeatureImpl.di.KEY_SERVICE_SHUTDOWN
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import org.koin.ktor.ext.inject
+import org.koin.core.qualifier.named
+import org.koin.ktor.ext.get
 
 fun Application.configureServerShutDown() {
 
-
     install(ShutDownUrl.ApplicationCallPlugin) {
-
-        val dbInstance by this@configureServerShutDown.inject<AppDatabaseFactory>()
-        dbInstance.close()
-
-        shutDownUrl = "/shutdown"
+        shutDownUrl = this@configureServerShutDown.get<String>(named(KEY_SERVICE_SHUTDOWN))
         exitCodeSupplier = { 0 }
     }
 
